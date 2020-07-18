5d87eb47a7ccbe6c6b5598ab2d5df0d4516b8d99
Aladin-Saleh <67257097+Aladin-Saleh@users.noreply.github.com>
2020-07-18 05:54:29 +0200

    Update and rename Timer to Timer.java

--- Timer.java
+++ Timer.java
@@ -3,7 +3,7 @@ public static void main(String[] args) {
              
         @Override
         public void run() {
-            System.out.println("exécuté toutes les secondes");             
+            //action            
         }
     };
          
